3695354de44ef4630499250e613f61af72144840
Sam Gallagher <gallaghersam95@gmail.com>
2023-01-13 15:07:52 -0600

    cleaned main

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_version = ">= 1.2.0"
-
-  backend "gcs" {
-    bucket = "tutorial-tf-state-001"
-    prefix = "best-tf-cd-article"
-  }
-}
--- terraform/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "google" {
-  project     = "sam-gallagher-tutorials-001"
-  region      = "us-central1"
-}
